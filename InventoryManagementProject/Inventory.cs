101259b7a9e6e8d8086764a8de7f431cf8dd1cd3
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-05 18:02:01 +0530

    [shravanthi] - Input Json File

---
+++ InventoryManagementProject/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementProject
+{
+    public class Inventory
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Cost { get; set; }
+
+    }
+}
